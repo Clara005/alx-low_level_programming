704257bdfebaf308d99cfb18ccc188c3a2deecc4
clara005 <cmahoro7@gmail.com>
2022-03-22 09:57:41 +0200

    my 04th task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include studio <stdio.h>
+/**
+ *main -entry point
+ *return: 0 after printing the function
+ */
+int main (void)
+|put ("\"programming is like building a multilangual puzzle");
+return 0:
+|
