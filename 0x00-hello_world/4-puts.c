2de4fffc4e8867faaf9770e48b2a3a0f8bb46b57
clara005 <cmahoro7@gmail.com>
2022-03-22 10:14:29 +0200

    my commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-#include studio <stdio.h>
+#include <stdio.h>
 /**
  *main -entry point
- *return: 0 after printing the function
- */
-int main (void)
-|put ("\"programming is like building a multilangual puzzle");
-return 0:
-|
+*
+*Return:0 after printing the function
+*/
+int main(void)
+{
+puts("\"programming is like a building a multilangual puzzle");
+return (0);
+}
